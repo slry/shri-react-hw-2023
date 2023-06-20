48f982481e061840975f0dcac40dc2a899a2f684
slry <v.rybalchenko@innopolis.university>
2023-06-21 01:50:06 +0300

    feat: add ui input

---
+++ biletopoisk/src/components/ui/UIInput/UIInput.tsx
@@ -0,0 +1,17 @@
+import useControlledInput from '@/hooks/useControlledInput'
+import styles from './UIInput.module.css'
+import { FunctionComponent, InputHTMLAttributes } from 'react';
+
+type UIInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  title: string;
+}
+
+export const UIInput: FunctionComponent<UIInputProps> = ({ title, ...props }) => {
+  const { value, onChange } = useControlledInput('');
+  return (
+    <div className={styles.wrap}>
+      <span className={styles.title}>{title}</span>
+      <input className={styles.input} value={value} onChange={onChange} {...props} />
+    </div>
+  )
+}
