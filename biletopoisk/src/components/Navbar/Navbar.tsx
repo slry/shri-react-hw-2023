b7998627edbc582f0403f363757f932075b1b728
slry <v.rybalchenko@innopolis.university>
2023-06-21 14:46:18 +0300

    style: prettier

--- biletopoisk/src/components/Navbar/Navbar.tsx
+++ biletopoisk/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
 import basket from '../../../public/basket.svg';
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <p className={styles.logo}>Билетопоиск</p>
-      <Image src={basket} alt="basket" />
-    </nav>
-  );
+	return (
+		<nav className={styles.navbar}>
+			<p className={styles.logo}>Билетопоиск</p>
+			<Image src={basket} alt="basket" />
+		</nav>
+	);
 };
-
